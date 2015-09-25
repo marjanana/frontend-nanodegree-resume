a9f99f382c46cb5e6c0cb1c0abef40d59ce94097
Ana Marjanovic <anamyers03@gmail.com>
2015-09-24 17:33:56 -0700

    vars

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,6 +20,49 @@ var bio = {
 
 var formattedName =
  HTMLheaderName.replace("%data%", bio.name);
+
 console.log(formattedName);
 $("#header").prepend(formattedName);
 
+var education = {
+  "schools":
+   [
+    "name": "Citi College of New York",
+    "location": "NYC",
+    "degree": "MA",
+    "majors":["Art History", "Museum Studies"],
+    "dates": 2011,
+    "url": "??"
+   ]
+
+  "onlineCourses":
+   [
+    "title": "Front-End DEveloper Nanodegree",
+    "school": "Udacity",
+    "date": "present",
+    "url": ""
+   ]
+};
+
+var work = {
+  "jobs":
+  [
+    "employer": "City College of New York",
+    "title": "Writing Tutor",
+    "location": "NYC",
+    "dates": "2014-2015",
+    "description":["Lorem Ipsum dolor sit amet."
+  ]
+};
+
+var projects = {
+  "projects":
+  [
+   "pr-1": {
+    "title": "lorem",
+    "dates":"2015-2015",
+    "description": "Lorem ipsum.",
+    "image": ""
+    }
+  ]
+}
