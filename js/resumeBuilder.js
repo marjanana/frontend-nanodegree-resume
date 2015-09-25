f545ad14a4f4ee1bd0bb6ae9c44dac92e5c9f830
Ana Marjanovic <anamyers03@gmail.com>
2015-09-24 17:12:36 -0700

    P2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,25 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var bio = {
+
+  "name": "Ana Marjanovic",
+  "role": "Web Developer",
+
+  "contacts":
+  {
+    "mobile": "646 326 4074",
+    "email": "anamyers03@gmail.com",
+    "github": "marjanana",
+    "location": "NYC"
+  },
+
+    "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "skills": ["lorem", "ipsum", "dolor"],
+    "biopic": "images/ja2.jpg",
+
+};
+
+var formattedName =
+ HTMLheaderName.replace("%data%", bio.name);
+console.log(formattedName);
+$("#header").prepend(formattedName);
+
