53f5763d54ba5557e22b2d714dd291af65fd0f21
Ana Marjanovic <anamyers03@gmail.com>
2015-09-24 17:56:03 -0700

    vars removed, header appended

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,49 +20,12 @@ var bio = {
 
 var formattedName =
  HTMLheaderName.replace("%data%", bio.name);
-
-console.log(formattedName);
 $("#header").prepend(formattedName);
 
-var education = {
-  "schools":
-   [
-    "name": "Citi College of New York",
-    "location": "NYC",
-    "degree": "MA",
-    "majors":["Art History", "Museum Studies"],
-    "dates": 2011,
-    "url": "??"
-   ]
+var formattedRole =
+ HTMLheaderRole.replace("%data%", bio.role);
+ $("#header").append(formattedRole);
 
-  "onlineCourses":
-   [
-    "title": "Front-End DEveloper Nanodegree",
-    "school": "Udacity",
-    "date": "present",
-    "url": ""
-   ]
-};
+console.log(formattedName);
 
-var work = {
-  "jobs":
-  [
-    "employer": "City College of New York",
-    "title": "Writing Tutor",
-    "location": "NYC",
-    "dates": "2014-2015",
-    "description":["Lorem Ipsum dolor sit amet."
-  ]
-};
 
-var projects = {
-  "projects":
-  [
-   "pr-1": {
-    "title": "lorem",
-    "dates":"2015-2015",
-    "description": "Lorem ipsum.",
-    "image": ""
-    }
-  ]
-}
