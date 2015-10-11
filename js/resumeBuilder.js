87304643c1289c4076fce9b5b9914f30f5255e74
Ana Marjanovic <anamyers03@gmail.com>
2015-10-10 23:52:33 -0700

    10/10/15

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,44 +3,60 @@ var bio = {
 
   "name": "Ana Marjanovic",
   "role": "Web Developer",
-
+  "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "biopic": "images/ja2.jpg",
   "contacts":
   {
-    "mobile": "646 326 4074",
+    "mobile": "646 555 5555",
     "email": "anamyers03@gmail.com",
     "github": "marjanana",
     "location": "NYC"
   },
-
-    "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "skills": ["lorem", "ipsum", "dolor"],
-    "biopic": "images/ja2.jpg",
-
-}
-
-var formattedName =
- HTMLheaderName.replace("%data%", bio.name);
-$("#header").prepend(formattedName);
-
-var formattedRole =
- HTMLheaderRole.replace("%data%", bio.role);
- $("#header").append(formattedRole);
-
-if(bio.skills.length > 0) {
-  $("#header").append(HTMLskillsStart);
-
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-  $("#skills").append(formattedSkill);
-
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-  $("#skills").append(formattedSkill);
-
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-  $("#skills").append(formattedSkill);
+    "skills": ["HTML", "CSS", "JavaScript", "Responsive Web Design"]
 };
+//name
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").append(formattedName);
+//role
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        $("#header").append(formattedRole);
+//message
+    var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+         $("#header").append(formattedMessage);
+//image
+    var formattedImage = HTMLbioPic.replace("%data%", bio.biopic);
+        $("#header").append(formattedImage);
+//skills
+if(bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
 
- var work = {
-  "jobs": [
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+        $("#skills").append(formattedSkill);
+
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+        $("#skills").append(formattedSkill);
+
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+        $("#skills").append(formattedSkill);
+
+     var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+        $("#skills").append(formattedSkill);
+}
+//contact
+for(contact in bio.contacts) {
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+       $("#footerContacts").prepend(formattedMobile, formattedEmail, formattedGitHub, formattedLocation);
+ };
+
+
+
+var work = {
+  "jobs":
+  [
   {
     "employer": "CCNY",
     "title": "Writing Tutor",
@@ -80,19 +96,20 @@ var projects = {
   "projects":
   [
     {
-    "title": "lorem",
+    "title": "Portfolio",
     "dates":"2015-2015",
     "description": "Lorem ipsum.",
-    "image": "test"
+    "image": "images/p1.jpg"
     },
-    {
-    "title": "lorem",
-    "dates":"2015-2015",
-    "description": "Lorem ipsum.",
-    "image": "test"
-    }
+    //{
+    // "title": "lorem",
+    // "dates":"2015-2015",
+    // "description": "Lorem ipsum.",
+    //"image": "images/p1.jpg"
+    //}
   ]
 };
+
 
 projects.display = function () {
   for (project in projects.projects) {
@@ -110,9 +127,12 @@ projects.display = function () {
     if (projects.projects[project].image.length > 0) {
 
         var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].image);
-        $(".project-entry:last").append(formattedImage);
+    $(".project-entry:last").append(formattedImage);
 
     }
   }
 };
 projects.display();
+
+
+$("#mapDiv").append(googleMap);
